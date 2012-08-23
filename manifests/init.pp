f5d43f97cf25d8f87670a2aa4e7f5d059ebb093f
Alessandro Franceschi <al@lab42.it>
2012-08-23 12:46:18 +0200

    Added Modulefile, lints and README updates

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,35 +1,43 @@
-# Experimental firewall define to abstract firewalling rules from the actual 
+# A generic firewall define to abstract firewalling rules from the actual
 # firewalling tool to use.
-# Currently only the "iptables# tool is supported, which makes use of Example42's
-# iptables module for host based local firewalling
+# Currently only the "iptables" tool is supported, which makes use of
+# Example42's iptables module for host based local firewalling
 #
 define firewall (
-  $source="",
-  $destination="",
-  $protocol="",
-  $port="",
-  $action="",
-  $direction="",
-  $tool="iptables",
-  $enable="true"
+  $source      = '',
+  $destination = '',
+  $protocol    = '',
+  $port        = '',
+  $action      = '',
+  $direction   = '',
+  $tool        = 'iptables',
+  $enable      = true
   ) {
 
   if ($tool =~ /iptables/) {
-    iptables::rule { "$name":
-      chain   => $direction ? {
-        "output" => "OUTPUT",       
-        default  => "INPUT",
-      },
-      target  => $action ? {
-        "deny"  => "DROP",       
-        default => "ACCEPT",
-      }, 
-      source    => $source,
+
+    $iptables_chain = $direction ? {
+      'output'  => 'OUTPUT',
+      'forward' => 'FORWARD',
+      default   => 'INPUT',
+    },
+
+    $iptables_target = $action ? {
+      'deny'  => 'DROP',
+      'drop'  => 'DROP',
+      default => 'ACCEPT',
+    },
+
+    iptables::rule { $name:
+      chain       => $iptables_chain,
+      target      => $iptables_target,
+      source      => $source,
       destination => $destination,
-      protocol  => $protocol,
-      port    => $port,
-      enable    => $enable,
+      protocol    => $protocol,
+      port        => $port,
+      enable      => $enable,
     }
+
   }
 
 }
