4ef2c1b5d91b26821822284d99ed6656b50c06b7
Jan Örnstedt <jan.ornstedt@postkodlotteriet.se>
2013-05-02 15:37:28 +0200

    Rewrote init file to use case statements instead of if then. I also added a default choice of firewall tool based on osfamily. Any chosen tool will override the default tool.

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,68 +13,81 @@ define firewall (
   $action         = '',
   $direction      = '',
   $order          = '',
-  $tool           = 'iptables',
+  $tool           = '',
   $enable         = true,
   $enable_v6      = false
   ) {
+  # Define standard firewall tool
+  $stdfw_tool = $::osfamily ? {
+  	/(?i:Solaris)/                                           => 'ipfilter',
+  	/(?i:Debian|Ubuntu|Mint|SLES|Gentoo|Mandrake|Archlinux)/ => 'iptables',
+  	default                                                  => '',
+  }
+  # Choose tool to use
+  $real_tool = $tool ? {
+  	''      => $stdfw_tool,
+  	default => $tool,
+  }
 
-  if ($tool =~ /iptables/) {
+  case $real_tool {
+    'iptables': {
+      $iptables_chain = $direction ? {
+        'output'  => 'OUTPUT',
+        'forward' => 'FORWARD',
+        default   => 'INPUT',
+      }
 
-    $iptables_chain = $direction ? {
-      'output'  => 'OUTPUT',
-      'forward' => 'FORWARD',
-      default   => 'INPUT',
+      $iptables_target = $action ? {
+        'deny'    => 'DROP',
+        'drop'    => 'DROP',
+        'reject'  => $protocol ? {
+          'tcp'   => 'REJECT --reject-with tcp-reset',
+          default => 'REJECT',
+        },
+        default   => 'ACCEPT',
+      }
+
+      iptables::rule { $name:
+        chain           => $iptables_chain,
+        target          => $iptables_target,
+        source          => $source,
+        source_v6       => $source_v6,
+        destination     => $destination,
+        destination_v6  => $destination_v6,
+        protocol        => $protocol,
+        port            => $port,
+        order           => $order,
+        enable          => $enable,
+        enable_v6       => $enable_v6,
+      }
     }
-
-    $iptables_target = $action ? {
-      'deny'    => 'DROP',
-      'drop'    => 'DROP',
-
-      'reject'  => $protocol ? {
-        'tcp'   => 'REJECT --reject-with tcp-reset',
-        default => 'REJECT',
-      },
-
-      default   => 'ACCEPT',
+    'ipfilter': {
+      $ipfilter_dir = $direction ? {
+        'output'  => 'out',
+        default   => 'in',
+      }
+      $ipfilter_action = $action ? {
+        'deny'    => 'block',
+        'drop'    => 'block',
+        'reject'  => 'block return-icmp-as-dest(3)',
+        default   => 'pass',
+      }
+      ipfilter::rule { $name:
+        direction       => $ipfilter_dir,
+        action          => $ipfilter_action,
+        source          => $source,
+        source_v6       => $source_v6,
+        destination     => $destination,
+        destination_v6  => $destination_v6,
+        protocol        => $protocol,
+        port            => $port,
+        order           => $order,
+        enable          => $enable,
+        enable_v6       => $enable_v6,
+      }
     }
-
-    iptables::rule { $name:
-      chain           => $iptables_chain,
-      target          => $iptables_target,
-      source          => $source,
-      source_v6       => $source_v6,
-      destination     => $destination,
-      destination_v6  => $destination_v6,
-      protocol        => $protocol,
-      port            => $port,
-      order           => $order,
-      enable          => $enable,
-      enable_v6       => $enable_v6,
-    }
-  } elsif ($tool =~ /ipfilter/) {
-  	
-    $ipfilter_dir = $direction ? {
-      'output'  => 'out',
-      default   => 'in',
-    }
-    $ipfilter_action = $action ? {
-      'deny'    => 'block',
-      'drop'    => 'block',
-      'reject'  => 'block return-icmp-as-dest(3)',
-      default   => 'pass',
-    }
-    ipfilter::rule { $name:
-      direction       => $ipfilter_dir,
-      action          => $ipfilter_action,
-      source          => $source,
-      source_v6       => $source_v6,
-      destination     => $destination,
-      destination_v6  => $destination_v6,
-      protocol        => $protocol,
-      port            => $port,
-      order           => $order,
-      enable          => $enable,
-      enable_v6       => $enable_v6,
+    default: {
+      fail("FIREWALL: Tool = ${tool} is not supported.")
     }
   }
 }
